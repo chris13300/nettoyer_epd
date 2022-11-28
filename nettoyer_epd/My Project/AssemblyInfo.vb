898cbfbbdd7e76a8eee8e21b41e8eafabf609720
Christophe JEAN <54830918+chris13300@users.noreply.github.com>
2022-11-28 18:30:20 +0100

    Add files via upload

---
+++ nettoyer_epd/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("nettoyer_epd")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("nettoyer_epd")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("770cc44e-fc0b-4025-8b6d-aef661299bb6")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ nettoyer_epd/modMain.vb
@@ -0,0 +1,52 @@
+﻿Module modMain
+
+    Sub Main()
+        Dim chaine As String, lecture As System.IO.StreamReader, mem As String, x As Integer, y As Integer, nb As Integer, tmp() As String
+        Dim nameEPD As String, fileTMP As String, start As Integer
+
+        nameEPD = My.Computer.FileSystem.GetName(Command())
+        If nameEPD = "" Then
+            Console.WriteLine("command : nettoyer_epd.exe your_epd_file.epd")
+            Console.ReadLine()
+            End
+        End If
+
+        fileTMP = Replace(Command(), ".epd", ".tmp")
+        If My.Computer.FileSystem.FileExists(fileTMP) Then
+            My.Computer.FileSystem.DeleteFile(fileTMP)
+        End If
+
+        y = Console.CursorTop
+        x = Console.CursorLeft
+
+        mem = ""
+        nb = 0
+        lecture = My.Computer.FileSystem.OpenTextFileReader(Command())
+        start = Environment.TickCount
+        While Not lecture.EndOfStream
+            chaine = lecture.ReadLine
+            If chaine = "" And mem <> "" Then
+                tmp = Split(mem, " ")
+                chaine = tmp(0) & " " & tmp(1) & " " & tmp(2) & " " & tmp(3) & vbCrLf
+                My.Computer.FileSystem.WriteAllText(fileTMP, chaine, True)
+                mem = ""
+                nb = nb + 1
+                If nb Mod 500 = 0 Then
+                    Console.SetCursorPosition(x, y)
+                    Console.Write("cleaning : " & nb & " positions (" & Format(nb / (Environment.TickCount - start) * 1000, "0") & " pos/sec)   ")
+                End If
+            Else
+                mem = chaine
+            End If
+        End While
+        lecture.Close()
+        lecture = Nothing
+        My.Computer.FileSystem.DeleteFile(Command())
+        My.Computer.FileSystem.RenameFile(fileTMP, nameEPD)
+        MsgBox(nb & " EPD positions !")
+
+        End
+
+    End Sub
+
+End Module
